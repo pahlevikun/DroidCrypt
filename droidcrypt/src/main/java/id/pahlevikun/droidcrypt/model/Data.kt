ea03d52154c032f63836bfc5a21f7c477733518e
Ivelhap <f.yuda.p69@gmail.com>
2018-10-06 18:04:34 +0700

    Move string to companion object for StoringManager

--- droidcrypt/src/main/java/id/pahlevikun/droidcrypt/model/Data.kt
+++ droidcrypt/src/main/java/id/pahlevikun/droidcrypt/model/Data.kt
@@ -2,7 +2,8 @@ package id.pahlevikun.droidcrypt.model
 
 import java.util.*
 
-data class Data(val textInString: String, val textInByteArray: ByteArray) {
+data class Data(val textInString: String,
+                val textInByteArray: ByteArray) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
